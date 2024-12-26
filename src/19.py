d68c3a2831323b9647c696fe7a1024323dea6d13
Matthew Green <mattgreen64@gmail.com>
2024-12-26 11:31:55 +0100

    Day 19 part 2 + refactoring

--- src/19.py
+++ src/19.py
@@ -1,26 +1,24 @@
 from helpers import get_input
 from itertools import groupby
 from functools import cache
+from collections import defaultdict
 
 @cache
-def check_subdesign(patterns, subdesign, max_pattern_length):
-    if subdesign in patterns:
-        return True
-    for l in range(1, max_pattern_length+1):
-        if subdesign[:l] in patterns:
-            return check_subdesign(patterns, subdesign[l:], max_pattern_length)
-    return False
+def design_possibilities(patterns, subdesign):
+    if 0 == len(subdesign):
+        return 1
+    return sum([design_possibilities(patterns, subdesign[len(p):]) for p in patterns if subdesign.startswith(p)])
 
-def part1(patterns, designs, max_pattern_length):
-    possible_designs = []
+def get_possible_designs(patterns, designs):
+    possible_designs = defaultdict(int)
     for design in designs:
-        if check_subdesign(patterns, design, max_pattern_length):
-            possible_designs.append(design)
-    print(f'Total number of possible designs: {len(possible_designs)}')
+        possible_designs[design] = design_possibilities(patterns, design)
+    return possible_designs
 
 lines = get_input('inputs/19.txt')
 patterns, designs = [list(sub) for elem, sub in groupby(lines, key = bool) if elem]
 patterns = [p.strip() for p in patterns[0].split(',')]
-max_pattern_length = max([len(p) for p in patterns])
 
-part1(tuple(patterns), designs, max_pattern_length)
+possible_designs = get_possible_designs(tuple(patterns), designs)
+print(f'Total number of possible designs: {sum(map(bool, possible_designs.values()))}')
+print(f'Total number of possible design permutations: {sum(possible_designs.values())}')
